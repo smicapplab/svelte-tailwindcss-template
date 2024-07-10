b4c3e7de854d5ac40c2f4b34d0fda6a5018f676c
Steve Torrefranca <s.torrefranca@gmail.com>
2024-07-10 19:14:00 +0800

    Fix Detected Vercel. Please remove adapter-static options to enable zero-config mode

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
